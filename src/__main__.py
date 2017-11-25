8f8f8e721817faed48a6e73030c9f298a982125d
RHYS SALDANHA <rhys.saldanha@gmail.com>
2017-11-25 16:16:41 +0100

    Move scrape code into function
    Complete functionality

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,22 @@
 import requests
+from typing import List
 
-url = 'http://commutelondon.com/CommuteLondon/CommuteLondon?getrequest=wordlistrelated&gettocname=VT+EAST+COAST'
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
-    # 'From': 'youremail@domain.com'  # This is another valid field
-}
+def get_trending_words(TOC: str) -> List[str]:
+    url = 'http://commutelondon.com/CommuteLondon/CommuteLondon?getrequest=wordlistrelated&gettocname={}'.format(TOC)
 
-response = requests.get(url, headers=headers)
-print(response.text)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/62.0.3202.94 Safari/537.36',
+    }
+
+    response = requests.get(url, headers=headers)
+    words = [line.split('!|!') for line in response.text.split('~|~')]
+
+    seen = set()
+    seen_add = seen.add
+    return [word[0] for word in words if word[0] != '' and not (word[0] in seen or seen_add(word[0]))]
+
+
+if __name__ == "__main__":
+    print(get_trending_words("VT+EAST+COAST"))
